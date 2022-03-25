9bdd98dc7c28458058d3869111d3e90964a48814
Chaaang <johnmarchangggggg@gmail.com>
2022-03-26 01:33:47 +0800

    feat: add local notification

--- pigs/lib/main.dart
+++ pigs/lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:pigs/firebase_options.dart';
+
 import 'package:pigs/screens/home.dart';
 import 'package:pigs/screens/settings.dart';
 import 'image_banner.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  final notification = message.notification;
+  final android = notification?.android;
+
+  if (notification != null && android != null) {
+    print("first notify");
+    print(notification.title);
+    print(notification.body);
+  }
+}
+
+//1
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', 'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.high);
+
+//2
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  //3
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
   runApp(const MyApp());
 }
 
@@ -34,6 +69,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    Content();
+    getToken();
+
+    super.initState();
+  }
+
+  void Content() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      final notification = message.notification;
+      final android = notification?.android;
+
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+            )));
+        print("2nd notify");
+        print("=====================");
+        print(notification.title);
+        print(notification.body);
+        print("=====================");
+      }
+    });
+  }
+
+  void getToken() async {
+    final token = await FirebaseMessaging.instance.getToken();
+    print("3nd notify");
+    print("=====================");
+    print(token);
+    print("=====================");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
